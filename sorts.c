f7d82785054a098389f289f4366926d4422b3aa0
Test1Denis <ddd183a_1@mail.ru>
2023-01-24 23:48:15 +0300

    надо доделать другие сортировки... и вынести в отдельные функции

--- sorts.c
+++ sorts.c
@@ -2,40 +2,40 @@
 using namespace std;
 #include "sorts.h"
 
+
 void insertionSort(int* ar,int  size){
-    int num;
+    int num;	//желательно все переменные инициализировать.....
     int pos;
     int con;
-    for(int i=1;i<size;i++){
-        if (ar[i]>ar[i-1]){
-            num=ar[i];
-            pos=i;
-            for(int j=pos;j>0;j--){
-                if (num>ar[j-1]){
-                    con=j-1;
+    for(int i = 1; i < size; i++){
+        if (ar[i] > ar[i-1]){
+            num = ar[i];
+            pos = i;
+            for(int j = pos; j > 0; j--){
+                if (num > ar[j-1]){
+                    con = j-1;
                 }
-                else{
+                else { //else плохо.... придумай как можно обойтись без else... маленькая подсказка: поменять условие....
                     break;
                 }
             }
-            for(int b=pos;b>con;b--){
-                ar[b]=ar[b-1];
+            for(int b = pos; b > con; b--){
+                ar[b] = ar[b-1];
             }
-            ar[con]=num;
+            ar[con] = num;
         }
     }
 }
 
-
-
 void bubbleSort(int* ar,int size){
     int num;
-    for(int s=size;s>0;s--){
-        for(int i=0;i<size-1;i++){
-            if (ar[i]<ar[i+1]){
+    for(int s = size; s > 0; s--){
+        for(int i = 0; i < size-1; i++){
+            if (ar[i] < ar[i+1]){
+//сделать функцию....
                 num = ar[i];
-                ar[i]=ar[i+1];
-                ar[i+1]=num;
+                ar[i] = ar[i+1];
+                ar[i+1] = num;
             }
         }
     }
